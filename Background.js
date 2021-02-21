b0003d6408d705995ff10a7a25bb6e07877ff3b8
sarv narayan <sarvnarayan12@gmail.com>
2021-02-21 12:51:38 +0530

    adding starting project

---
+++ Background.js
@@ -0,0 +1,25 @@
+var css = document.querySelector("h3")
+var color1=document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body=document.getElementById("gradient")
+
+// console.log(css)
+// console.log(color1)
+// console.log(color2)
+
+function setGradient(){
+    body.style.background=
+    "linear-gradient(to right,"
+     +color1.value +", "
+      +color2.value + 
+      ")";
+      css.textContent=body.style.background + ";";
+      console.log(body.style.background);
+}
+
+
+
+
+color1.addEventListener("input",setGradient)
+
+color2.addEventListener("input",setGradient)
